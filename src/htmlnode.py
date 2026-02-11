9cd401c088abe4e379b0601fd697193598315549
FreakHuhn <mcbabamuck@googlemail.com>
2026-02-11 17:46:36 +0100

    wrote a child class for converting leafNodes, also for ParentNodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,3 +25,37 @@ class HtmlNode:
             props_str += f' {key}="{value}"'
         
         return props_str
+    
+class LeafNode(HtmlNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+    
+    def __repr__(self):
+        return f"HtmlNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+    
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode must have a value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HtmlNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+        self.tag = tag
+        self.children = children
+        self.props = props 
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+        children_html = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,7 +4,7 @@ Docstring for test_htmlnode
 Create some tests for the HTMLNode class (at least 3). I used a new file called src/test_htmlnode.py. Create a few nodes and make sure the props_to_html method works as expected.
 """
 import unittest
-from htmlnode import HtmlNode
+from htmlnode import HtmlNode , LeafNode, ParentNode
 
 class TestHtmlNode(unittest.TestCase):
     def test_props_to_html_no_props(self):
@@ -18,6 +18,32 @@ class TestHtmlNode(unittest.TestCase):
     def test_props_to_html_with_empty_props(self):
         node = HtmlNode(tag="span", props={})
         self.assertEqual(node.props_to_html(), "")
-        
-        
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("Hello, world!", "p")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("Click here", "a", props={"href": "https://www.example.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.example.com">Click here</a>')
+    
+    def test_to_html_with_children(self):
+        child_node = LeafNode("child", "span")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("grandchild", "b")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+    )
+    def test_to_html_without_children(self):
+        parent_node = ParentNode("div", [])
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
+            
+            
 if __name__ == "__main__":    unittest.main()
