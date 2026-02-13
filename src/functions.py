d29293f861efa41f67095eef13821028d4cae664
FreakHuhn <mcbabamuck@googlemail.com>
2026-02-13 22:18:44 +0100

    added function to split markdown text into TextNode's based on images

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,5 @@
 import re
-from textnode import TextNode, TextType
+from textnode import *
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -17,7 +17,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_nodes.append(TextNode(part, text_type))
     return new_nodes
-#should return a list of tuples with the alt text and url of the images in the markdown text
+
 def extract_markdown_images(text):
     pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(pattern, text)
@@ -33,3 +33,25 @@ def extract_markdown_links(text):
     for anchor_text, url in matches:
         links.append((anchor_text, url))
     return links
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        images = extract_markdown_images(node.text)
+        if not images:
+            new_nodes.append(node)
+            continue
+        text = node.text
+        for alt_text, url in images:
+            image_markdown = f"![{alt_text}]({url})"
+            if image_markdown in text:
+                parts = text.split(image_markdown)
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                new_nodes.append(TextNode(alt_text, TextType.IMAGE, url=url))
+                text = parts[1]
+        if text:
+            new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
